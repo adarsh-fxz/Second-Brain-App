35d0513953961adfeed3253e833b491bd0d53d34
Adarsh Gupta <gadarsh318@gmail.com>
2025-02-13 03:36:21 +0545

    Add Link Schema

--- src/db.ts
+++ src/db.ts
@@ -13,7 +13,14 @@ const ContentSchema = new Schema({
     title: String,
     link: String,
     tags: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
 })
 
 export const ContentModel = model('Content', ContentSchema);
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true},
+})
+
+export const LinkModel = model('Link', LinkSchema);
